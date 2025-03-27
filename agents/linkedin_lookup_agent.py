23e7d6626ab3fc9172787c7dfe159dc4fb879831
NG <mavenishere@gmail.com>
2025-03-27 21:23:26 +0530

    making progress - engy

--- agents/linkedin_lookup_agent.py
+++ agents/linkedin_lookup_agent.py
@@ -7,6 +7,8 @@ from langchain.tools import Tool
 from langchain.agents import AgentExecutor, create_react_agent
 from langsmith import Client
 
+from tools.tools import get_profile_url_tavily
+
 
 def lookup_linkedin_profile(profile_name: str) -> str:
     llm = ChatOllama(
@@ -15,8 +17,15 @@ def lookup_linkedin_profile(profile_name: str) -> str:
         temperature=0,
     )
     template = """
-    Give a person name {name_of_person}, I want you to provide me with link to their proifle page on LinkedIn platform.
-    The answer should only contain a link in valid url format "http(s)://<link-to-person-linkedin-profile-page>"
+    You are a professional LinkedIn researcher.
+
+    Your task is to find the **exact LinkedIn profile URL** of the person named: "{name_of_person}".
+
+    🛑 Instructions:
+    - Respond ONLY with the final answer in the format:
+    Final Answer: https://www.linkedin.com/in/username
+    - Do NOT include any explanation, introduction, or extra text.
+    - Do NOT return company or group links—only personal profile links.
     """
 
     prompt_template = PromptTemplate(
@@ -27,22 +36,40 @@ def lookup_linkedin_profile(profile_name: str) -> str:
         Tool(
             name="Crawl Google for linkedIn Page",
             description="Useful when you want to get the LinkedIn page url.",
-            func="?",
+            func=get_profile_url_tavily,
         )
     ]
 
     # Create a LANGSMITH_API_KEY in Settings > API Keys
     client = Client(api_key=os.environ["LANGSMITH_API_KEY"])
     react_prompt = client.pull_prompt("hwchase17/react", include_model=True)
+
     react_agent = create_react_agent(
         llm=llm, tools=tools_for_agent, prompt=react_prompt
     )
-    result = AgentExecutor(
+    agent_executor = AgentExecutor(
         agent=react_agent,
         verbose=True,
         metadata={"why": "it matters"},
         tools=tools_for_agent,
     )
 
+    result = agent_executor.invoke(
+        input={
+            "input": PromptTemplate.format_prompt(
+                self=react_prompt,
+                tools=tools_for_agent,
+                tool_names="ToolA, ToolB",
+                input=profile_name,
+                agent_scratchpad="",
+            )
+        },
+    )
+
     linkedin_profile_url = result["output"]
     return linkedin_profile_url
+
+
+if __name__ == "__main__":
+    profile_url = lookup_linkedin_profile("Eden Marco")
+    print(profile_url)
--- agents/tools/tools.py
+++ agents/tools/tools.py
@@ -6,7 +6,7 @@ load_dotenv()
 
 def get_profile_url_tavily(username: str) -> str:
     search = TavilySearchResults(
-        max_results=20,
+        max_results=5,
         include_answer=True,
     )
     search_result = search.run(verbose=True, tool_input=username)
